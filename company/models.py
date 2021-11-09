3daa85f7692e05f9a50abb5d8ad4b769bfe893a4
Eunhye Park <eunhyepark@Eunhyeui-MacBookPro.local>
2021-11-09 13:32:57 +0900

    Add: create models.py
    - 테이블 4개(Company, CompanyTag, Tag, CompanyName) 생성 모델링

--- company/models.py
+++ company/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    tags         = models.ManyToManyField('Tag', through='CompanyTag', blank=True)
+    company_name = models.OneToOneField('CompanyName', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'companies'
+
+class CompanyTag(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    tag     = models.ForeignKey('Tag', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'companytags'
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'tags'
+
+class CompanyName(models.Model):
+    name = models.CharField(max_length=100)
+    
+    class Meta:
+        db_table = 'company_names'
