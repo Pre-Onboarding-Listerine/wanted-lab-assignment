072db04d2e7d775c8d6e8617414097f11b6ca904
Gouache-studio <zoobellmint@khu.ac.kr>
2021-11-10 05:37:20 +0900

    ADD : CompanyCreateView, models.py, coreapp

--- company/models.py
+++ company/models.py
@@ -1,27 +1,48 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-class Company(models.Model):
-    tags         = models.ManyToManyField('Tag', through='CompanyTag', blank=True)
-    company_name = models.OneToOneField('CompanyName', on_delete=models.CASCADE)
-    
+from core.models import TimeStampModel
+
+class Company(TimeStampModel):
+    company_name = models.JSONField(max_length=128, null=True)
+
     class Meta:
         db_table = 'companies'
 
-class CompanyTag(models.Model):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    tag     = models.ForeignKey('Tag', on_delete=models.CASCADE)
+class Tag(TimeStampModel):
+    tag_name = models.JSONField(max_length=100, null=True)
+    company  = models.ForeignKey('Company',on_delete=CASCADE)
 
-    class Meta:
-        db_table = 'companytags'
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
-    
     class Meta:
         db_table = 'tags'
 
-class CompanyName(models.Model):
-    name = models.CharField(max_length=100)
-    
-    class Meta:
-        db_table = 'company_names'
+
+
+# class CompanyName(TimeStampModel):
+#     name     = models.CharField(max_length=128, null=True)
+#     language = models.CharField(max_length=4, null=True)
+#     company  = models.ForeignKey('Company', on_delete=CASCADE)
+
+#     class Meta:
+#         db_table = 'company_names'
+
+# class Company(TimeStampModel):
+#     tags = models.ManyToManyField('Tag', through='CompanyTag', null=True)
+
+#     class Meta:
+#         db_table = 'companies'
+
+# class CompanyTag(models.Model):
+#     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+#     tag     = models.ForeignKey('Tag', on_delete=models.CASCADE)
+
+#     class Meta:
+#         db_table = 'companytags'
+
+# class Tag(TimeStampModel):
+#     name     = models.CharField(max_length=100, null=True)
+#     language = models.CharField(max_length=4, null=True)
+
+#     class Meta:
+#         db_table = 'tags'
+
